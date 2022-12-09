af7e2f4cdbf91cb41834e00b50cf64c0a7965977
Sandul Renuja <sandulrenuja@gmail.com>
2022-12-09 19:33:50 +0530

    Add CustomNavbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import CustomNavbar from './components/CustomNavbar';
 import React from "react";
 
 function App() {
   return (
     <div className="App">
+      <CustomNavbar/>
     </div>
   );
 }
