b4882ece922e40294e6b20c3d2c08244af7fb0b4
Sandul Renuja <sandulrenuja@gmail.com>
2022-12-12 02:16:31 +0530

    Add hero section of second page

---
+++ src/pages/SecondPage.jsx
@@ -0,0 +1,13 @@
+import CustomNavbar from "../components/common/CustomNavbar";
+import HeroSection2 from "../components/page-2/HeroSection2";
+
+const SecondPage = () => {
+  return (
+    <>
+      <CustomNavbar/>
+      <HeroSection2/>
+    </>
+  );
+}
+
+export default SecondPage
