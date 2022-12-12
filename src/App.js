ff44f193beaf852d57ba2df110f0c3063b92928d
Sandul Renuja <sandulrenuja@gmail.com>
2022-12-12 22:10:54 +0530

    Install and configure react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.scss';
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SecondPage from "./pages/SecondPage";
 import ThirdPage from "./pages/ThirdPage";
+import Layout from "./Layout";
 
 function App() {
   return (
-    <div className="App">
-      <ThirdPage/>
-    </div>
+    <main className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<HomePage/>}/>
+            <Route path="about-us" element={<SecondPage/>}/>
+            <Route path="our-services" element={<ThirdPage/>}/>
+            <Route path="*" element={<HomePage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
--- src/pages/ThirdPage.jsx
+++ src/pages/ThirdPage.jsx
@@ -1,5 +1,3 @@
-import CustomNavbar from "../components/common/CustomNavbar";
-import Footer from "../components/common/Footer";
 import HeroSection3 from "../components/page-3/HeroSection3";
 import OurServicesSection from "../components/page-3/OurServicesSection";
 import OurPartnersSection from "../components/page-3/OurPartnersSection";
@@ -7,11 +5,9 @@ import OurPartnersSection from "../components/page-3/OurPartnersSection";
 const ThirdPage = () => {
   return (
     <>
-      <CustomNavbar/>
       <HeroSection3/>
       <OurServicesSection/>
       <OurPartnersSection/>
-      <Footer/>
     </>
   );
 }
