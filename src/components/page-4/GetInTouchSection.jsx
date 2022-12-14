e8601b831131088490922c1cdddde6ff5f06c363
Sandul Renuja <sandulrenuja@gmail.com>
2022-12-14 14:48:49 +0530

    Update Get-in-touch form with primereact components

--- src/components/page-4/GetInTouchSection.jsx
+++ src/components/page-4/GetInTouchSection.jsx
@@ -1,118 +1,134 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import {InputText} from 'primereact/inputtext';
+import {MultiSelect} from 'primereact/multiselect';
+import {InputTextarea} from "primereact/inputtextarea";
 
 const GetInTouchSection = () => {
-  return (
-    <section className="get-in-touch-section pt-1 px-5 pb-3">
-      <Container className="py-2 px-5">
-        <Row sm={12} className="justify-content-center text-center pt-4 pb-4">
-          <Col sm={12}>
-            <h2 className="section-title mb-4">Get in touch</h2>
-          </Col>
-        </Row>
-        <Row sm={12} className="card-wrapper py-3 my-3 justify-content-center gap-5">
-          <Col sm={12} md={10}>
-            <Form>
-              <Row className="mb-2">
-                <Col sm={12}>
-                  <Form.Group className="mb-3" controlId="subject">
-                    <Form.Label>What Can We Help You With?</Form.Label>
-                    <Form.Select size="lg" multiple={true}>
-                      <option value=""></option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
-                    </Form.Select>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row className="mb-2">
-                <Col sm={12} md={6}>
-                  <Form.Group className="mb-3" controlId="first-name">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text"/>
-                  </Form.Group>
-                </Col>
-                <Col sm={12} md={6}>
-                  <Form.Group className="mb-3" controlId="last-name">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text"/>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row className="mb-2">
-                <Col sm={12} md={6}>
-                  <Form.Group className="mb-3" controlId="email-address">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email"/>
-                  </Form.Group>
-                </Col>
-                <Col sm={12} md={6}>
-                  <Form.Group className="mb-3" controlId="phone-no">
-                    <Form.Label>Phone Number</Form.Label>
-                    <Form.Control type="text"/>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row className="mb-2">
-                <Col sm={12} md={6}>
-                  <Form.Group className="mb-3" controlId="job-title">
-                    <Form.Label>Job Title</Form.Label>
-                    <Form.Control type="text"/>
-                  </Form.Group>
-                </Col>
-                <Col sm={12} md={6}>
-                  <Form.Group className="mb-3" controlId="company-name">
-                    <Form.Label>Company Name</Form.Label>
-                    <Form.Control type="text"/>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row className="mb-2">
-                <Col sm={12} md={6}>
-                  <Col sm={12} md={6}>
-                    <Form.Group className="mb-3" controlId="subject">
-                      <Form.Label>Company Size</Form.Label>
-                      <Form.Select>
-                        <option value=""></option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </Form.Select>
-                    </Form.Group>
-                  </Col>
-                </Col>
-                <Col sm={12} md={6}>
-                  <Form.Group className="mb-3" controlId="subject">
-                    <Form.Label>How did you hear about Reamer Made?</Form.Label>
-                    <Form.Select>
-                      <option value=""></option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
-                    </Form.Select>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row className="mt-4 mb-2">
-                <Col sm={12}>
-                  <Form.Group className="mb-3" controlId="message">
-                    <Form.Label>Tell Us About Your Project</Form.Label>
-                    <Form.Control as="textarea" rows={8}/>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row className="justify-content-center mb-1">
-                <Col sm={12} md={6}>
-                  <Button variant="primary" className="mt-3 mb-3 w-75">Book Now</Button>
-                </Col>
-              </Row>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
+  const [formData, setFormData] = useState({
+    servicesRequested: "",
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    phoneNumber: "",
+    jobTitle: "",
+    companyName: "",
+    companySize: "",
+    referral: "",
+    message: "",
+  });
+
+  const servicesFieldOptions = ['Media', 'Web Development', 'Digital Marketing', 'Branding & Creative', 'Other'];
+  const companySizeFieldOptions = ['0-29', '30-59', '60-149', '150-249', '250+'];
+  const referralFieldOptions = ['Social media', 'Email or newsletter', 'Search engine', 'Word of mouth', 'In-person events', 'Other'];
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+
+  return (<section className="get-in-touch-section pt-1 px-5 pb-3">
+    <Container className="py-0 px-5">
+      <Row sm={12} className="justify-content-center text-center pt-4 pb-4">
+        <Col sm={12}>
+          <h2 className="section-title mb-4">Get in touch</h2>
+        </Col>
+      </Row>
+      <Row sm={12} className="card-wrapper py-3 my-3 justify-content-center gap-5">
+        <Col sm={12} md={10}>
+          <Form>
+            <Row className="mb-2">
+              <Col sm={12}>
+                <Form.Group className="mb-3">
+                  <Form.Label>What Can We Help You With?</Form.Label>
+                  <MultiSelect showSelectAll={true} filter={false} placeholder={"Choose services..."}
+                               name="servicesRequested" value={formData.servicesRequested}
+                               scrollHeight={"300px"} options={servicesFieldOptions}
+                               onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mb-2">
+              <Col sm={12} md={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>First Name</Form.Label>
+                  <InputText name="firstName" value={formData.firstName} onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Last Name</Form.Label>
+                  <InputText name="lastName" value={formData.lastName} onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mb-2">
+              <Col sm={12} md={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Email Address</Form.Label>
+                  <InputText name="emailAddress" type="email" value={formData.emailAddress} onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Phone Number</Form.Label>
+                  <InputText name="phoneNumber" type="text" value={formData.phoneNumber} onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mb-2">
+              <Col sm={12} md={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Job Title</Form.Label>
+                  <InputText name="jobTitle" type="text" value={formData.jobTitle} onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Company Name</Form.Label>
+                  <InputText name="companyName" type="text" value={formData.companyName} onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mb-2">
+              <Col sm={12} md={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Company Size</Form.Label>
+                  <MultiSelect showSelectAll={false} filter={false} placeholder={"Choose company size"}
+                               name="companySize" value={formData.companySize}
+                               selectionLimit={1}
+                               scrollHeight={"300px"} options={companySizeFieldOptions}
+                               onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>How Did You Hear About Reamer Made?</Form.Label>
+                  <MultiSelect showSelectAll={true} filter={false} placeholder={"Choose option..."}
+                               name="referral" value={formData.referral}
+                               scrollHeight={"300px"} options={referralFieldOptions}
+                               maxSelectedLabels={3}
+                               onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mt-3 mb-2">
+              <Col sm={12}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Tell Us About Your Project?</Form.Label>
+                  <InputTextarea rows={5} cols={30} value={formData.message} onChange={handleChange}/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="justify-content-center mb-1">
+              <Col sm={12} md={6}>
+                <Button variant="primary" className="mt-3 mb-3 w-75">Book Now</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  </section>);
 }
 
 export default GetInTouchSection;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import 'primeicons/primeicons.css';
+// import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
