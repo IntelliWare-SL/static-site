39e3f9d3f9f70e5f8c4d18defa04b35030487fec
Sandul Renuja <sandulrenuja@gmail.com>
2022-12-19 02:32:44 +0530

    Refactor components and complete all components

--- src/components/page-1/FeaturedProjectsSection.jsx
+++ src/components/page-1/FeaturedProjectsSection.jsx
@@ -1,5 +1,6 @@
 import {Col, Container, Row} from "react-bootstrap";
 import React from "react";
+import PhotoGallery from "../common/PhotoGallery";
 
 const FeaturedProjectsSection = () => {
   return (
@@ -10,41 +11,7 @@ const FeaturedProjectsSection = () => {
             <h2 className="section-title">Featured Projects</h2>
           </Col>
         </Row>
-        <Row className="photo-row flex-wrap py-4 px-2 px-md-0">
-          <Col xs={4} className="px-0">
-            <div className="window" id="photo-1">
-              <img src="https://www.dfwurbanrealty.com/uploads/bigstock-external-view-of-a-contemporar-61564541_650.jpg"
-                   alt=""/>
-            </div>
-            <div className="window" id="photo-2">
-              <img
-                src="https://img.freepik.com/premium-photo/interior-exterior-design-pool-villa-which-features-living-area_41487-217.jpg?w=2000"
-                alt=""/>
-            </div>
-          </Col>
-          <Col xs={4} className="px-0">
-            <div className="window" id="photo-3">
-              <img src="https://img.freepik.com/free-photo/house-isolated-field_1303-23773.jpg?w=2000"
-                   alt=""/>
-            </div>
-            <div className="window" id="photo-4">
-              <img
-                src="https://img.freepik.com/premium-photo/3d-rendering-upscale-modern-mansion-with-pool_190619-546.jpg?w=2000"
-                alt=""/>
-            </div>
-          </Col>
-          <Col xs={4} className="px-0">
-            <div className="window" id="photo-5">
-              <img src="https://img.freepik.com/premium-photo/3d-rendering-modern-house_62754-1884.jpg?w=2000"
-                   alt=""/>
-            </div>
-            <div className="window" id="photo-6">
-              <img
-                src="https://img.freepik.com/premium-photo/3d-rendering-luxury-modern-building-village-night_105762-1209.jpg?w=2000"
-                alt=""/>
-            </div>
-          </Col>
-        </Row>
+        <PhotoGallery/>
       </Container>
     </section>
   );
--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -1,12 +1,13 @@
 import HeroSection3 from "../components/page-3/HeroSection3";
 import OurServicesSection from "../components/page-3/OurServicesSection";
+import OtherServicesSection from "../components/page-3/OtherServicesSection";
 
 const Page3 = () => {
   return (
     <>
       <HeroSection3/>
       <OurServicesSection/>
-      {/*<OurPartnersSection/>*/}
+      <OtherServicesSection/>
     </>
   );
 }
