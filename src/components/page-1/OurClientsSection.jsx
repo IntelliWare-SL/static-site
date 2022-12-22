f9930fb6437667a6661d3fd8b060bb5c9f7dea06
kasun7153 <kasun7153@gmail.com>
2022-12-22 12:00:59 +0530

    Map update

--- src/components/page-1/OurClientsSection.jsx
+++ src/components/page-1/OurClientsSection.jsx
@@ -2,6 +2,7 @@ import {Col, Container, Row} from "react-bootstrap";
 import React from "react";
 import CustomButton from "../common/CustomButton";
 import ClientsMap from "../../../src/assets/images/clients-map.png";
+import { Slide, Fade } from "react-awesome-reveal";
 
 const OurClientsSection = () => {
   return (
@@ -9,10 +10,13 @@ const OurClientsSection = () => {
       <Container className="pt-4 pb-4 px-1 px-md-0">
         <Row className="flex-column-reverse flex-md-row px-2 px-md-0">
           <Col xs={12} md={6} xxl={6} className="flex px-0 pe-md-4">
-            <img src={ClientsMap}
-                 className="clients-map px-0 px-md-2 py-1 my-2 pt-sm-3 mt-sm-3 pt-md-5 mt-md-5 pt-lg-2 mt-lg-2 pt-xl-3"
-                 alt="Map of our clients" width="100%"/>
+            <Fade duration={3000}>
+              <img src={ClientsMap}
+                   className="clients-map px-0 px-md-2 py-1 my-2 pt-sm-3 mt-sm-3 pt-md-5 mt-md-5 pt-lg-2 mt-lg-2 pt-xl-3"
+                   alt="Map of our clients" width="100%"/>
+            </Fade>
           </Col>
+
           <Col xs={12} md={6} xxl={6}
                className="d-flex flex-column px-0 ps-md-4 justify-content-center pb-4">
             <h2 className="mt-2 mb-1 section-title">Our Clients</h2>
